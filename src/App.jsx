117a380118c5fec14d29bd32ebefe30994fc91e6
Ana Morandeira <ana.agrodorolo@gmail.com>
2026-02-19 00:37:26 +0100

    refactor: rename Registro to Register and update App routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Registro from './pages/Register';
+import Register from './pages/Register'; // Asegúrate de que apunte al nuevo nombre del archivo
 import Categories from './pages/Categories';
 import Alerts from './pages/Alerts';
+
 function App() {
   return (
     <Router>
-      {/* Contenedor principal que simula un móvil */}
       <div className="max-w-md mx-auto min-h-screen bg-white shadow-2xl overflow-hidden font-sans">
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* Cambiamos el <div> por el componente real */}
-          <Route path="/registro" element={<Register />} /> 
-          
+          <Route path="/register" element={<Register />} /> 
           <Route path="/categories" element={<Categories />} />
           <Route path="/alerts" element={<Alerts />} />
         </Routes>
