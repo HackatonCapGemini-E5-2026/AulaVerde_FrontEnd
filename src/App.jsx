f222ddeb2daa39f2ecbe56b8b9fff6891b3618fd
javiertunsi7 <javiertunsi7@gmail.com>
2026-02-19 11:25:35 +0100

    add the import

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Register from './pages/Register'; // Asegúrate de que apunte al nuevo nombre del archivo
+import Register from './pages/Register';
 import Categories from './pages/Categories';
 import Alerts from './pages/Alerts';
+import { ThemeProvider } from './ThemeContext';
 
 function App() {
   return (
-    <Router>
-      <div className="max-w-md mx-auto min-h-screen bg-white shadow-2xl overflow-hidden font-sans">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} /> 
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/alerts" element={<Alerts />} />
-        </Routes>
-      </div>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <div className="max-w-md mx-auto min-h-screen shadow-2xl overflow-hidden font-sans">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/alerts" element={<Alerts />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
